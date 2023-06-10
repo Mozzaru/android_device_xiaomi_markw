3ab988b122eeb6d9e9598c27112fb0850634912d
mozzaru <rianyanuar52@gmail.com>
2023-06-11 05:44:14 +0700

    Revert "markw: adapt for Spark"
    This reverts commit 3a22f0b3d4f611ed8c97928a81783fca18c70a3c.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_markw.mk
+    $(LOCAL_DIR)/lineage_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_markw-user \
-    spark_markw-userdebug \
-    spark_markw-eng
+    lineage_markw-user \
+    lineage_markw-userdebug \
+    lineage_markw-eng
