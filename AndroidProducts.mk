30a2592292f4b6e70e5e856aab2e87dcbec00a9c
mozzaru <rianyanuar52@gmail.com>
2023-06-11 05:52:09 +0700

    markw: adapt for Spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_markw.mk
+    $(LOCAL_DIR)/spark_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_markw-user \
-    lineage_markw-userdebug \
-    lineage_markw-eng
+    spark_markw-user \
+    spark_markw-userdebug \
+    spark_markw-eng
