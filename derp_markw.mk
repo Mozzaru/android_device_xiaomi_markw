26e85d0cee0fe84e0065059476e4b4a7fb47dfec
mozzaru <rianyanuar52@gmail.com>
2023-07-07 23:57:13 +0700

    bootanimation: Make it work on all devices

--- derp_markw.mk
+++ derp_markw.mk
@@ -30,7 +30,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
-USE_LEGACY_BOOTANIMATION := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := markw
